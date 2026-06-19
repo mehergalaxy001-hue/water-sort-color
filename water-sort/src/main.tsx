d4e1cf0cef134c68823d16121f54a9b05fb7e4ba
mehergalaxy001-hue <mehergalaxy001@gmail.com>
2026-06-19 08:44:24 +0530

    fix CrazyGames SDK integration issue

--- water-sort/src/main.tsx
+++ water-sort/src/main.tsx
@@ -1,10 +1,28 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode, useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+function CrazyInit() {
+  useEffect(() => {
+
+    const init = async () => {
+      try {
+        await window.CrazyGames.SDK.init();
+      } catch (e) {}
+
+      window.CrazyGames.SDK.game.gameStart();
+    };
+
+    init();
+
+  }, []);
+
+  return <App />;
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <CrazyInit />
   </StrictMode>,
 );
